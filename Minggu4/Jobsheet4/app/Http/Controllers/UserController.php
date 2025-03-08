f825239c07a696b8445ed8dd5114667c17c20560
RezaAngelinaFebriyanti <rzangef15@gmail.com>
2025-03-08 15:05:27 +0700

    P2.1 Retrieving Single Models

---
+++ Minggu4/Jobsheet4/app/Http/Controllers/UserController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        //findOr
+        /*
+        $user = UserModel::findOr(1, ['username', 'nama'], function() {
+            abort(404);
+        });
+        */
+        $user = UserModel::findOr(20, ['username', 'nama'], function() {
+            abort(404);
+        });
+        /*
+        $data = [
+            'username' => 'customer-1',
+            'nama' => 'Pelanggan',
+            'password' => Hash::make('12345'),
+            'level_id' => 4
+        ];
+        UserModel::insert($data);
+        */
+
+        /*
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data);
+        */
+
+        //DATA UNTUK $fillable
+        /*
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_dua',
+            'nama' => 'Manager 2',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
+        */
+
+        /*
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
+        */
+        
+        // Akses model UserModel
+        //$user = UserModel::all();
+
+        //RETRIEVING SINGLE MODELS
+        /*
+        $user = UserModel::find(1);
+        $user = UserModel::where('level_id', 1)->first();
+        $user = UserModel::firstWhere('level_id', 1);
+        */
+        return view('user', ['data' => $user]);
+    }
+}
