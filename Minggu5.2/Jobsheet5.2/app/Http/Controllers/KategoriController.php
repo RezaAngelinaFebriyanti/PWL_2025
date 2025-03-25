806050f1b8873a032b04708070b314b022eb5cbd
RezaAngelinaFebriyanti <rzangef15@gmail.com>
2025-03-25 14:17:25 +0700

    Blade View, Web Temp, AdminLTE

---
+++ Minggu5.2/Jobsheet5.2/app/Http/Controllers/KategoriController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KategoriModel;
+use Illuminate\Http\Request;
+use App\DataTables\KategoriDataTable;
+
+class KategoriController extends Controller
+{
+    //Praktikum2
+    public function index(KategoriDataTable $dataTable)
+    {
+        return $dataTable->render('kategori.index');
+    }
+
+    //Praktikum3
+    public function create() {
+        return view('kategori.create');
+    }
+
+    public function store(Request $request)
+    {
+        KategoriModel::create([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori,
+        ]);
+
+        return redirect('/kategori');
+    }
+
+    //Praktikum 4
+    public function edit($id)
+    {
+        $data = KategoriModel::findOrFail($id);
+        return view('kategori.edit', ['kategori' => $data]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'kodeKategori' => 'required',
+            'namaKategori' => 'required'
+        ]);
+
+        //cari data berdasarkan id
+        $kategori = KategoriModel::findOrFail($id);
+
+        //update data
+        $kategori->kategori_kode = $request->kodeKategori;
+        $kategori->kategori_nama = $request->namaKategori;
+        $kategori->save();
+
+        return redirect('/kategori');
+    }
+
+    public function delete($id) {
+        KategoriModel::where('kategori_id', $id)->delete();
+        return redirect('/kategori');
+    }
+}
