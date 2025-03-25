17c550026d7e05614ae7d359ad367cb3e9939855
RezaAngelinaFebriyanti <rzangef15@gmail.com>
2025-03-25 19:33:05 +0700

    Tugas m_level

--- Minggu5.2/Jobsheet5.2/app/Models/LevelModel.php
+++ Minggu5.2/Jobsheet5.2/app/Models/LevelModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class LevelModel extends Model
 {
@@ -13,4 +14,9 @@ class LevelModel extends Model
     protected $primaryKey = 'level_id'; // Mendefinisikan primary key dari tabel yang digunakan
 
     protected $fillable = ['level_kode', 'level_nama']; // Mendefinisikan kolom yang bisa diisi oleh user
+
+    //m_level
+    public function users():HasMany {
+        return $this->hasMany(UserModel::class, 'level_id', 'level_id');
+    }
 }
--- Minggu5.2/Jobsheet5.2/routes/web.php
+++ Minggu5.2/Jobsheet5.2/routes/web.php
@@ -63,3 +63,15 @@ Route::group(['prefix' => 'user'], function () {
     Route::put('/{id}', [UserController::class, 'update']); // menyimpan perubahan data user
     Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
 });
+
+//m_level
+Route::group(['prefix' => 'level'], function () {
+    Route::get('/', [UserController::class, 'index']); // menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']); // menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']); // menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']); // menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']); // menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']); // menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']); // menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
+});
