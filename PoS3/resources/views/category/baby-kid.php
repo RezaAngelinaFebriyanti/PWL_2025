3ae5692dd83a1e5e2e25a5a519fe51aaed8091a5
RezaAngelinaFebriyanti <rzangef15@gmail.com>
2025-03-29 22:28:36 +0700

    PoS Jobsheet 3

---
+++ PoS3/resources/views/category/baby-kid.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Baby & Kids Stuff</title>
+</head>
+<body>
+    <h3>Produk Perlengkapan Bayi dan Anak Lapakku</h3>
+    <h1>Minyak Telon: Rp 35.000</h1>
+    <h1>Minyak Rambut: Rp 105.000</h1>
+</body>
+</html>
---
+++ PoS3/resources/views/category/home-care.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home-care</title>
+</head>
+<body>
+    <h3>Product Home Care Lapakku</h3>
+    <h1>Pewangi Lantai: Rp 25.000</h1>
+    <h1>Pengharum Ruangan: Rp. 55.000</h1>
+</body>
+</html>
