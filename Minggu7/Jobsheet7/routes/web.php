3a6ad23b16b1a5a5ebb4be2fc5c3288c27e7cd9e
RezaAngelinaFebriyanti <rzangef15@gmail.com>
2025-04-06 09:35:03 +0700

    Tugas 1

--- Minggu7/Jobsheet7/routes/web.php
+++ Minggu7/Jobsheet7/routes/web.php
@@ -63,7 +63,7 @@ Route::pattern('id', '[0-9]+');
 
 Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'postlogin']);
-Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
 
 Route::middleware(['auth'])->group(function() {
     Route::get('/', [WelcomeController::class, 'index']);
