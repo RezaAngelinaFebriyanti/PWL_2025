998b0d8d75338a62aba9534ab3063ef4bad99b61
RezaAngelinaFebriyanti <rzangef15@gmail.com>
2025-04-10 21:15:51 +0700

    Route Import

--- Minggu5.2/Jobsheet5.2/routes/web.php
+++ Minggu5.2/Jobsheet5.2/routes/web.php
@@ -68,14 +68,14 @@ Route::group(['prefix' => 'user'], function () {
 
 //m_level
 Route::group(['prefix' => 'level'], function () {
-    Route::get('/', [UserController::class, 'index']); // menampilkan halaman awal user
-    Route::post('/list', [UserController::class, 'list']); // menampilkan data user dalam bentuk json untuk datatables
-    Route::get('/create', [UserController::class, 'create']); // menampilkan halaman form tambah user
-    Route::post('/', [UserController::class, 'store']); // menyimpan data user baru
-    Route::get('/{id}', [UserController::class, 'show']); // menampilkan detail user
-    Route::get('/{id}/edit', [UserController::class, 'edit']); // menampilkan halaman form edit user
-    Route::put('/{id}', [UserController::class, 'update']); // menyimpan perubahan data user
-    Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
+    Route::get('/', [LevelController::class, 'index']); // menampilkan halaman awal level
+    Route::post("/list", [LevelController::class, 'list']); // menampilkan data level dalam bentuk json untuk datatables
+    Route::get('/create', [LevelController::class, 'create']); // menampilkan halaman form tambah level
+    Route::post('/', [LevelController::class, 'store']); // menyimpan data level baru
+    Route::get('/{id}', [LevelController::class, 'show']); // menampilkan detail level
+    Route::get('/{id}/edit', [LevelController::class, 'edit']); // menampilkan halaman form edit level
+    Route::put('/{id}', [LevelController::class, 'update']); // menyimpan perubahan data level
+    Route::delete('/{id}', [LevelController::class, 'destroy']); // menghapus data level
 });
 
 //m_kategori
