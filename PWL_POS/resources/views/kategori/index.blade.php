32f8f4dfbe3a480b5e2ab1b6979ac53efeba353e
RezaAngelinaFebriyanti <rzangef15@gmail.com>
2025-04-12 14:55:09 +0700

    POS untuk UTS

---
+++ PWL_POS/resources/views/kategori/index.blade.php
@@ -0,0 +1,114 @@
+@extends('layouts.template')
+
+@section('content')
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">{{ $page->title }}</h3>
+        <div class="card-tools">
+            <button onclick="modalAction('{{ url('/kategori/import') }}')" class="btn btn-info">Import</button>
+            <a href="{{ url('/kategori/export_excel') }}" class="btn btn-primary">
+                <i class="fa fa-file-excel"></i>
+                Export Kategori (.xlsx)
+            </a>
+            <a href="{{ url('/kategori/export_pdf') }}" class="btn btn-warning">
+                <i class="fa fa-file-excel"></i>
+                Export Kategori (.pdf)
+            </a>
+            <button onclick="modalAction('{{ url('/kategori/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
+        </div>
+    </div>
+
+    <div class="card-body">
+        {{-- Filter --}}
+        <div id="filter" class="form-horizontal filter-date p-2 border-bottom mb-2">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group form-group-sm row text-sm mb-0">
+                        <label for="filter_date" class="col-md-1 col-form-label">Filter</label>
+                        <div class="col-md-3">
+                            <select name="filter_kategori" class="form-control form-control-sm filter_kategori">
+                                <option value="">- Semua -</option>
+                            </select>
+                            <small class="form-text text-muted">Kategori Barang</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {{-- Flash Message --}}
+        @if(session('success'))
+            <div class="alert alert-success">{{ session('success') }}</div>
+        @endif
+
+        @if(session('error'))
+            <div class="alert alert-danger">{{ session('error') }}</div>
+        @endif
+
+        <table class="table table-bordered table-hover table-sm" id="table_kategori">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Kode Kategori</th>
+                    <th>Nama Kategori</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+        </table>
+    </div>
+</div>
+<div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data- backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div>
+@endsection
+
+@push('js')
+    <script>
+        function modalAction(url = ''){
+            $('#myModal').load(url,function(){
+            $('#myModal').modal('show')
+        });
+    }
+
+    var dataKategori;
+    $(document).ready(function() {
+        dataKategori = $('#table_kategori').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: {
+                url: "{{ url('kategori/list') }}",
+                dataType: "json",
+                type: "POST",
+                data: function(d) {
+                    d.kategori_id = $('.filter_kategori').val();
+                }
+            },
+            columns: [
+                {
+                    data: "DT_RowIndex", // nomor urut dari laravel datatable addIndexColumn()
+                    className: "text-center",
+                    orderable: false,
+                    searchable: false
+                },
+                {
+                    data: "kategori_kode",
+                    orderable: true,
+                    searchable: true
+                },
+                {
+                    data: "kategori_nama",
+                    orderable: true,
+                    searchable: true
+                },
+                {
+                    data: "aksi",
+                    orderable: false,
+                    searchable: false
+                }
+            ]
+        });
+        $('#kategori_id').on('change', function() {
+            dataKategori.ajax.reload();
+        });
+    });
+</script>
+@endpush
