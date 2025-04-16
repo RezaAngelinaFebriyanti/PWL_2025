cfc8cb21241510ed499fa4e93caf13f5d6201569
RezaAngelinaFebriyanti <rzangef15@gmail.com>
2025-04-16 09:31:48 +0700

    Edit ajax stok

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -205,11 +205,15 @@ Route::middleware(['auth'])->group(function() {
         Route::post('/import_ajax', [StokController::class, 'import_ajax']); // ajax import excel
         Route::get('/export_excel', [StokController::class, 'export_excel']); // ajax form upload excel
         Route::get('/export_pdf', [StokController::class, 'export_pdf']); // ajax form upload pdf
-        Route::get('/{id}', [StokController::class, 'show']); // menampilkan detail stok
+        Route::get('/{id}/edit_ajax', [StokController::class, 'edit_ajax']); // Menampilkan halaman form edit stok Ajax
+        Route::put('/{id}/update_ajax', [StokController::class, 'update_ajax']); // Menyimpan perubahan data stok Ajax
+        Route::get('/{id}/show_ajax', [StokController::class, 'show_ajax']); // menampilkan detail stok 
     });
 
     Route::group(['prefix' => 'penjualan'], function () {
         Route::get('/', [PenjualanController::class, 'index']); // menampilkan halaman awal stok
         Route::post('/list', [PenjualanController::class, 'list']); // menampilkan data stok dalam bentuk json untuk datatables
+        Route::get('/import', [PenjualanController::class, 'import']); // ajax form upload excel
+        Route::post('/import_ajax', [PenjualanController::class, 'import_ajax']); // ajax import excel
     });
 });
