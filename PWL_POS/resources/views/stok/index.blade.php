8edf1db1925043f76da70eaec7ea40d8741c9c3f
RezaAngelinaFebriyanti <rzangef15@gmail.com>
2025-04-16 16:56:51 +0700

    pembetulan tanda `

--- PWL_POS/resources/views/stok/index.blade.php
+++ PWL_POS/resources/views/stok/index.blade.php
@@ -5,7 +5,7 @@
     <div class="card-header">
         <h3 class="card-title">Daftar Stok Barang</h3>
         <div class="card-tools">
-        <button onclick="modalAction('{{ url('/stok/import') }}')" class="btn btn-info">Import Stok</button>
+        <button onclick="modalAction(`{{ url('/stok/import') }}`)" class="btn btn-info">Import Stok</button>
             <a href="{{ url('/stok/export_excel') }}" class="btn btn-primary">
                 <i class="fa fa-file-excel"></i>
                 Export Stok (.xlsx)
@@ -14,7 +14,7 @@
                 <i class="fa fa-file-excel"></i>
                 Export Stok (.pdf)
             </a>
-        <button onclick="modalAction('{{ url('/stok/create_ajax') }}')" class="btn btn-success">Tambah Stok</button>
+        <button onclick="modalAction(`{{ url('/stok/create_ajax') }}`)" class="btn btn-success">Tambah Stok</button>
         </div>
     </div>
 
