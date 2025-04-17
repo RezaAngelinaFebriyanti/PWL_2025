9a2bafcac469e46729afd4c8b5adf9a8a9d27117
RezaAngelinaFebriyanti <rzangef15@gmail.com>
2025-04-17 09:57:02 +0700

    add upload gambar profil: menu Profile

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -11,6 +11,7 @@ use App\Models\LevelModel;
 use Illuminate\Support\Facades\Validator;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
@@ -478,5 +479,72 @@ class UserController extends Controller
         $pdf->render();
 
         return $pdf->stream('Data User ' . date('Y-m-d H:i:s') . '.pdf');
+    }
+
+    public function profile()
+    {
+        $user = auth()->user();
+
+        if (!$user) {
+            return redirect('/login')->with('error', 'Silahkan login terlebih dahulu');
+        }
+
+        $breadcrumb = (object) [
+            'title' => 'Profile User',
+            'list' => ['Home', 'Profile']
+        ];
+
+        $page = (object) [
+            'title' => 'Profil Pengguna'
+        ];
+
+
+        $activeMenu = 'profile'; // Set menu aktif
+
+        return view('user.profile', compact('user', 'breadcrumb', 'page', 'activeMenu'));
+    }
+
+    public function updatePhoto(Request $request)
+    {
+        // Validasi file
+        $request->validate([
+            'profile_photo' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+        ]);
+
+        try {
+            // auth user yang login
+            $user = auth()->user();
+
+            if (!$user) {
+                return redirect('/login')->with('error', 'Silahkan login terlebih dahulu');
+            }
+
+            // user id
+            $userId = $user->user_id;
+
+            $userModel = UserModel::find($userId);
+
+            if (!$userModel) {
+                return redirect('/login')->with('error', 'User tidak ditemukan');
+            }
+
+            // hapus jika sudah ada foto profile
+            if ($userModel->profile_photo && file_exists(storage_path('app/public/' . $userModel->profile_photo))) {
+                Storage::disk('public')->delete($userModel->profile_photo);
+            }
+
+            // update foto profile baru 
+            $fileName = 'profile_' . $userId . '_' . time() . '.' . $request->profile_photo->extension();
+            $path = $request->profile_photo->storeAs('profiles', $fileName, 'public');
+
+            // Update
+            UserModel::where('user_id', $userId)->update([
+                'profile_photo' => $path
+            ]);
+
+            return redirect()->back()->with('success', 'Foto profile berhasil diperbarui');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Gagal mengupload foto: ' . $e->getMessage());
+        }
     }
 }
