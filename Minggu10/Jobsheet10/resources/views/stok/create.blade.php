b06054408b8d255706fa647d3887626905643195
RezaAngelinaFebriyanti <rzangef15@gmail.com>
2025-04-26 09:52:14 +0700

    Jobsheet 10

---
+++ Minggu10/Jobsheet10/resources/views/stok/create.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.template')
+
+@section('content')
+<div class="card card-outline card-primary">
+    <div class="card-header">
+        <h3 class="card-title">{{ $page->title }}</h3>
+        <div class="card-tools"></div>
+    </div>
+    <div class="card-body">
+        <form method="POST" action="{{ url('stok') }}" class="form-horizontal">
+            @csrf
+            <div class="form-group row">
+                <label class="col-1 control-label col-form-label">ID Barang</label>
+                <div class="col-11">
+                    <select class="form-control" id="barang_id" name="barang_id" required>
+                        <option value="">- Pilih ID Barang -</option>
+                        @foreach($barang as $l)
+                            <option value="{{ $l->barang_id }}">{{ $l->barang_nama }}</option>
+                        @endforeach
+                    </select>
+                    @error('barang_id')
+                        <small class="form-text text-danger">{{ $message }}</small>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label class="col-1 control-label col-form-label">ID User</label>
+                <div class="col-11">
+                    <select class="form-control" id="user_id" name="user_id" required>
+                        <option value="">- Pilih ID User -</option>
+                        @foreach($user as $l)
+                            <option value="{{ $l->user_id }}" {{ $l->user_id == $stok->user_id ? 'selected' : '' }}>
+                                {{ $l->nama }}
+                            </option>
+                        @endforeach
+                    </select>
+                    @error('user_id')
+                        <small class="form-text text-danger">{{ $message }}</small>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label class="col-1 control-label col-form-label">Jumlah</label>
+                <div class="col-11">
+                    <input type="text" class="form-control" id="username" name="username" value="{{ old('jumlah') }}" required>
+                    @error('jumlah')
+                        <small class="form-text text-danger">{{ $message }}</small>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label class="col-1 control-label col-form-label"></label>
+                <div class="col-11">
+                    <button type="submit" class="btn btn-primary btn-sm">Simpan</button>
+                    <a class="btn btn-sm btn-default ml-1" href="{{ url('user') }}">Kembali</a>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
+
+@push('css')
+@endpush
+
+@push('js')
+@endpush
---
+++ Minggu10/Jobsheet10/resources/views/stok/index.blade.php
@@ -0,0 +1,146 @@
+@extends('layouts.template')
+
+@section('content')
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">Daftar Stok Barang</h3>
+        <div class="card-tools">
+        <button onclick="modalAction(`{{ url('/stok/import') }}`)" class="btn btn-info">Import Stok</button>
+            <a href="{{ url('/stok/export_excel') }}" class="btn btn-primary">
+                <i class="fa fa-file-excel"></i>
+                Export Stok (.xlsx)
+            </a>
+            <a href="{{ url('/stok/export_pdf') }}" class="btn btn-warning">
+                <i class="fa fa-file-pdf"></i>
+                Export Stok (.pdf)
+            </a>
+        <button onclick="modalAction(`{{ url('/stok/create_ajax') }}`)" class="btn btn-success">Tambah Stok</button>
+        </div>
+    </div>
+
+    <div class="card-bord">
+        {{-- Filter --}}
+        <div id="filter" class="form-horizontal filter-date p-2 border-bottom mb-2">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group form-group-sm row text-sm mb-0">
+                        <label for="filter_date" class="col-md-1 col-form-label">Filter</label>
+                        <div class="col-md-3">
+                            <select name="filter_stok" class="form-control form-control-sm filter_stok">
+                                <option value="">- Semua -</option>
+                                @foreach($barang as $l)
+                                    <option value="{{ $l->barang_id }}">{{ $l->barang_nama }}</option>
+                                @endforeach
+                            </select>
+                            <small class="form-text text-muted">Stok Barang</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @if(session('success'))
+            <div class="alert alert-success">{{ session('success') }}</div>
+        @endif
+
+        @if(session('error'))
+            <div class="alert alert-danger">{{ session('error') }}</div>
+        @endif
+
+        <table class="table table-bordered table-sm table-striped table-hover" id="table-stok" width="100%">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>ID Barang</th>
+                    <th>Nama Barang</th>
+                    <th>Stok</th>
+                    <th>User</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+        </table>
+    </div>
+</div>
+
+<div id="myModal" class="modal fade animate shake" tabindex="-1" data-backdrop="static" data-keyboard="false" data-width="75%"></div>
+@endsection
+
+@push('js')
+<script>
+    function modalAction(url = '') {
+        $('#myModal').load(url, function() {
+            $('#myModal').modal('show');
+        });
+    }
+
+    var tableStok;
+    $(document).ready(function() {
+        tableStok = $('#table-stok').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: {
+                url: "{{ url('stok/list') }}",
+                type: "POST",
+                data: function (d) {
+                    d.barang_id = $('.filter_stok').val();
+                }
+            },
+            columns: [
+                { 
+                    data: 'DT_RowIndex',
+                    className: 'text-center',
+                    width: '5%',
+                    orderable: false,
+                    searchable: false
+                },
+                { 
+                    data: 'barang_id',
+                    className: 'text-center',
+                    width: '15%',
+                    orderable: true,
+                    searchable: true
+                },
+                { 
+                    data: 'barang_nama',
+                    className: '',
+                    width: '15%',
+                    orderable: true,
+                    searchable: true
+                },
+                { 
+                    data: 'jumlah',
+                    className: 'text-center',
+                    width: '10%',
+                    orderable: true,
+                    searchable: true
+                },
+                { 
+                    data: 'user_nama', 
+                    className: '',
+                    width: '20%',
+                    orderable: true,
+                    searchable: true 
+                },
+                { 
+                    data: 'aksi', 
+                    className: 'text-center', 
+                    width: '15%', 
+                    orderable: false, 
+                    searchable: false 
+                }
+            ]
+
+        });
+
+        $('#table-stok_filter input').unbind().bind().on('keyup', function(e) {
+            if (e.keyCode == 13) {
+                tableStok.search(this.value).draw();
+            }
+        });
+
+        $('.filter_stok').change(function() {
+            tableStok.ajax.reload();
+        });
+    });
+</script>
+@endpush
