d6074209691e8e1e1bf857d3d74e01b7a99267d8
RezaAngelinaFebriyanti <rzangef15@gmail.com>
2026-03-28 19:40:37 +0700

    Routing: Raute Parameters

--- Minggu2/Jobsheet2/routes/web.php
+++ Minggu2/Jobsheet2/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route; //Mengimpor class Route
 use App\Http\Controllers\ItemController; //Tambahan: Mengimpor class ItemController
 
 //BASIC ROUTING
+/*
 // 1.
 Route::get('/hello', function() {
     return 'Hello World';
@@ -22,4 +23,18 @@ Route::get('/SelamatDatang', function () {
 // 4.
 Route::get('/about', function () {
     return 'NIM: 2341760015 <br> Nama: Reza Angelina Febriyanti';
+});
+*/
+
+// ROUTER PARAMETERS
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/article/{id}', function ($postId) {
+    return 'Halaman Artikel dengan ID '.$postId;
 });
