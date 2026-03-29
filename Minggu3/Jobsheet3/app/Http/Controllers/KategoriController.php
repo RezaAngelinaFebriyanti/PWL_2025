985e7e3bd881de97d74427f63d8c2163a36d11b2
RezaAngelinaFebriyanti <rzangef15@gmail.com>
2026-03-29 10:30:32 +0700

    QUERY BUILDER: Implementasi Query Builder

--- Minggu3/Jobsheet3/app/Http/Controllers/KategoriController.php
+++ Minggu3/Jobsheet3/app/Http/Controllers/KategoriController.php
@@ -9,26 +9,22 @@ class KategoriController extends Controller
 {
     public function index()
     {
-        /*
+        // Query Builder
         $data = [
             'kategori_kode' => 'SNK',
             'kategori_nama' => 'Snack/Makanan Ringan',
             'created_at' => now(), 
         ];
-        DB::table('m_kategori')->insert($data);
+        DB::table('m_kategori')->insert($data); // insert
         return 'Insert data baru berhasil';
-        */
 
-        /*
         $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
-        return 'Update data berhasil. Jumlah data yg diupdate: ' .$row. ' baris';
-        */
+        return 'Update data berhasil. Jumlah data yg diupdate: ' .$row. ' baris'; // update
 
+        $row= DB::table('m_kategori') -> where('kategori_kode', 'SNK')->delete(); //delete
+        return 'Delete data berhasil. Jumlah data yg dihapus: ' .$row. ' baris';
 
-        //$row= DB::table('m_kategori') -> where('kategori_kode', 'SNK')->delete();
-        //return 'Delete data berhasil. Jumlah data yg dihapus: ' .$row. ' baris';       
-        
         $data = DB::table('m_kategori')->get();
-        return view('kategori', ['data' => $data]);
+        return view('kategori', ['data' => $data]); //menampilkan isi tabel
     }
 }
